74b29c59dab68087994b6070eda6e0105dd7921a
JorgeLpzGnz <geolg763@gmail.com>
2023-02-17 10:48:53 -0500

    add arrays library comments

--- contracts/libraries/Arrays.sol
+++ contracts/libraries/Arrays.sol
@@ -1,10 +1,12 @@
-// SPDX-License-Identifier: AGPL-3.0-only
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
+/// @title Arrays a library for uint256 arrays
+/// @notice a library to add array methods
 library Arrays {
 
+    /// @notice returns the index of the given element
+    /// @dev it will reject the tx if the element doesn't exist
     function indexOf( uint[] memory array, uint element ) internal pure returns ( uint index ) {
 
         for ( uint256 i = 0; i < array.length; i++ ) {
@@ -13,8 +15,15 @@ library Arrays {
 
         }
 
+        // if the function has not returned anything it means that the 
+        // element does not exist, so it will be rejected
+
+        require( true, "The element doesn't exist");
+
     }
 
+    /// @notice returns a boolean indicating whether it is included or not
+    /// @return included true = included, false = not included
     function includes(uint[] memory array, uint element ) internal pure returns ( bool included ) {
 
         for ( uint256 i = 0; i < array.length; i++ ) {
@@ -25,9 +34,14 @@ library Arrays {
 
     }
 
+    /// @notice it removes the element of the passed 
+    /// @dev to remove the element, just take the last item in
+    /// the array and set it to the index of the item to be removed,
+    /// then remove the last item
+    /// @return true if the element was deleted
     function remove( uint[] storage array, uint index ) internal returns( bool ) {
 
-        if ( index > array.length ) return false;
+        if ( index > array.length - 1 ) return false;
 
         array[ index ] = array[ array.length - 1 ];
 
@@ -36,4 +50,5 @@ library Arrays {
         return true;
 
     }
+
 }
